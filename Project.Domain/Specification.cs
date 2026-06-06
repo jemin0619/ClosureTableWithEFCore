09b381152702f4d6300b2b607fcb11450ffc21a6
jemin0619 <jemin200619@gmail.com>
2026-06-06 19:14:21 +0900

    초기 커밋.

---
+++ Project.Domain/Specification.cs
@@ -0,0 +1,33 @@
+﻿namespace Project.Domain;
+
+public class Specification
+{
+    public string Name { get; set; }
+    public InnerSpecA InnerSpecA { get; set; }
+    public InnerSpecB InnerSpecB { get; set; }
+}
+
+public class InnerSpecA
+{
+    public int A { get; set; }
+    public int B { get; set; }
+}
+
+public class InnerSpecB
+{
+    public string C { get; set; }
+    public string D { get; set; }
+    public InnerSpecC InnerSpecC { get; set; }
+}
+
+public class InnerSpecC
+{
+    public SpecType Type { get; set; }
+}
+
+public enum SpecType
+{
+    TypeA,
+    TypeB,
+    TypeC
+}
